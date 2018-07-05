e7ac00661c05a4f8cad810fb314a60ed88898e0f
mohammed zaid <zaid@asareri.com>
2018-07-05 20:38:58 +0530

    firstcommit

---
+++ resources/views/test.blade.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+<form action="create.php" method="post">
+first_name:<br><input type="text" name="first_name" value=" "><br>
+last_name:<br><input type="text" name="last_name" value=" "><br>
+email_id:<br><input type="text" name="email_id" value=""><br>
+date_of_birth:<br><input type="text" name="date_of_birth" value=""><br>
+<br><input type="submit" value="submit"><br>
+</form>
+</body>
+</html>
